b0eff26db2990a70895fd0124d7d3061a5872637
nagai <nagai.hayate.76r@st.kyoto-u.ac.jp>
2018-07-05 05:05:54 +0900

    Part3 done
    パート三終了。退出メソッドを実装

---
+++ Assets/Roomfor1/GameManager1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Com.MyCompany.MyGame
+{
+    public class GameManager1 : MonoBehaviour//=>1
+    {
+
+        #region Photon Messages
+
+        /// <summary>
+        /// Called when the local player left the room. We need to load the launcher scene.
+        /// </summary>
+        public void OnLeftRoom()//void void => void
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void LeaveRoom()
+        {
+            PhotonNetwork.LeaveRoom();
+        }
+
+        #endregion
+    }
+}
